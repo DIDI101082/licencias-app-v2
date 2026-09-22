2dd5add4ac6765acdb8a54946602d38cea464cf9
DIDI101082 <alejandro.dandrea@accusys.com.ar>
2026-09-22 19:40:02 -0300

    Change Mark component to use an image instead of SVG

--- components/Mark.tsx
+++ components/Mark.tsx
@@ -1,8 +1,9 @@
-export default function Mark({ className = "h-6 w-6" }: { className?: string }) {
+export default function Mark({ className = "h-7" }: { className?: string }) {
   return (
-    <svg viewBox="0 0 24 24" className={className} fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M4 18L10 6H14L8 18H4Z" fill="#2F5CF0" />
-      <path d="M9.5 18L15.5 6H19.5L13.5 18H9.5Z" fill="#7EA1F8" />
-    </svg>
+    <img
+      src="/accusys-logo.png"
+      alt="Accusys Technology"
+      className={`${className} !w-auto object-contain`}
+    />
   );
 }
